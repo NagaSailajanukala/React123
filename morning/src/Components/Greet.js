f120b45e0816f8265a360ce231856ce0d7b198d6
NagaSailajanukala <iamnukala@outlook.com>
2023-12-05 15:44:43 -0500

    morning react app is added

---
+++ morning/src/Components/Greet.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function Greet(){
+    return <h1>Good Morning All</h1>
+}
+export default Greet;
---
+++ morning/src/Components/wish.js
@@ -0,0 +1,5 @@
+import React from 'react'
+function wish(){
+    return (<h1>whishing all the best</h1>)
+}
+export default wish;
